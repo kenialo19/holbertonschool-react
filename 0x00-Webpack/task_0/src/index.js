f251c2e89bf57850e16fc7be6f03da94a4a7adf9
kenialo19 <3396@holbertonschool.com>
2022-05-09 12:44:58 -0500

    modified filwe

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('Copyright - Holberton School');
